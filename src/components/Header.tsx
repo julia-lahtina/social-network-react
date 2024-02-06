f7678d007af1694c9210a9e25bfa4d060218831e
julia-lahtina <j.lakhtin@gmail.com>
2024-02-06 18:26:29 +0100

    add new components

---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const Header = () => {
+    return (
+            <header className={"header"}>
+                <img src="https://cdn.worldvectorlogo.com/logos/batman-30538.svg" alt=""/>
+            </header>
+    );
+};
+
