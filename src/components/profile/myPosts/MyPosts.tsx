90573f0aba6ac619993d1771920c9cf3eadc01ed
julia-lahtina <j.lakhtin@gmail.com>
2024-02-14 21:48:58 +0100

    add new component

--- src/components/profile/myPosts/MyPosts.tsx
+++ src/components/profile/myPosts/MyPosts.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import s from './MyPosts.module.css';
+import {Post} from './post/Post';
 
 export const MyPosts = () => {
     return (
@@ -10,18 +10,8 @@ export const MyPosts = () => {
                 <button>Add post</button>
             </div>
             <div className={'posts'}>
-                <div className={s.item}>
-                    <img
-                        src={'https://www.fr.de/assets/images/30/586/30586280-3d-bueste-einer-figur-aus-avatar-2WsZTh7umIec.jpg'}
-                        alt={'avatar'}/>
-                    post 1
-                </div>
-                <div className={s.item}>
-                    <img
-                        src={'https://www.fr.de/assets/images/30/586/30586280-3d-bueste-einer-figur-aus-avatar-2WsZTh7umIec.jpg'}
-                        alt={'avatar'}/>
-                    post 2
-                </div>
+                <Post message={'Hi, how are you?'}/>
+                <Post message={"It's my first post"}/>
             </div>
         </div>
     );
