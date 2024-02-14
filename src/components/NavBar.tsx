c19cbe81fedfb6d9a84840552b2e7eb7aa798fc6
julia-lahtina <j.lakhtin@gmail.com>
2024-02-14 19:33:15 +0100

    add import css file

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,21 +1,21 @@
 import React from 'react';
-
+import "./NavBar.css";
 export const NavBar = () => {
     return (
         <nav className={"nav"}>
-            <div>
+            <div className={"item"}>
                 <a href="">Profile</a>
             </div>
-            <div>
+            <div className={"item"}>
                 <a href="">Message</a>
             </div>
-            <div>
+            <div className={"item"}>
                 <a href="">News</a>
             </div>
-            <div>
+            <div className={"item"}>
                 <a href="">Music</a>
             </div>
-            <div>
+            <div className={"item"}>
                 <a href="">Settings</a>
             </div>
         </nav>
