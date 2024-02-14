3f41d8a91d9219f713e93493e05f6f674958fbe9
julia-lahtina <j.lakhtin@gmail.com>
2024-02-14 21:49:25 +0100

    change

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
-import {Header} from "./components/header/Header";
-import {NavBar} from "./components/navBar/NavBar";
-import {Profile} from "./components/profile/Profile";
+import {Header} from './components/header/Header';
+import {NavBar} from './components/navBar/NavBar';
+import {Profile} from './components/profile/Profile';
 
 function App() {
     return (
-        <div className="app-wrapper">
+        <div className='app-wrapper'>
 
             <Header/>
             <NavBar/>
