c4627f05a02282bfc1cb16d9e2f21bdefe71be50
julia-lahtina <j.lakhtin@gmail.com>
2024-02-14 20:15:54 +0100

    add new classes

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,22 +1,22 @@
 import React from 'react';
-import "./NavBar.css";
+import s from './NavBar.module.css'
 export const NavBar = () => {
     return (
-        <nav className={"nav"}>
-            <div className={"item"}>
-                <a href="">Profile</a>
+        <nav className={s.nav}>
+            <div className={s.item}>
+                <a>Profile</a>
             </div>
-            <div className={"item"}>
-                <a href="">Message</a>
+            <div className={s.item}>
+                <a>Message</a>
             </div>
-            <div className={"item"}>
-                <a href="">News</a>
+            <div className={s.item}>
+                <a>News</a>
             </div>
-            <div className={"item"}>
-                <a href="">Music</a>
+            <div className={s.item}>
+                <a>Music</a>
             </div>
-            <div className={"item"}>
-                <a href="">Settings</a>
+            <div className={s.item}>
+                <a>Settings</a>
             </div>
         </nav>
     );
