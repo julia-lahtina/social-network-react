08dbcf56f52abcb76ead3a50d20f861821af6bd3
julia-lahtina <j.lakhtin@gmail.com>
2024-02-14 20:55:52 +0100

    rename imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import './App.css';
-import {Header} from "./components/Header/Header";
-import {NavBar} from "./components/NavBar/NavBar";
-import {Profile} from "./components/Profile/Profile";
+import {Header} from "./components/header/Header";
+import {NavBar} from "./components/navBar/NavBar";
+import {Profile} from "./components/profile/Profile";
 
 function App() {
     return (
