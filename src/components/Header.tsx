20e35349c4d1b1031ea5d57c292089ad8bd96eb3
julia-lahtina <j.lakhtin@gmail.com>
2024-02-14 20:50:00 +0100

    add new classes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import s from './Header.module.css'
 
+
 export const Header = () => {
     return (
             <header className={s.header}>
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import s from './NavBar.module.css'
 
+console.log(s)
+
 export const NavBar = () => {
     return (
         <nav className={s.nav}>
             <div className={s.item}>
                 <a>Profile</a>
             </div>
-            <div className={s.item}>
+            <div className={`${s.item} ${s.active}`}>
                 <a>Message</a>
             </div>
             <div className={s.item}>
