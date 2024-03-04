9c256c9c9d2d46f42c7e460fd009d474d3583bfb
julia-lahtina <j.lakhtin@gmail.com>
2024-03-04 20:28:42 +0100

    add Route

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,24 @@ import {Header} from './components/header/Header';
 import {NavBar} from './components/navBar/NavBar';
 import {Profile} from './components/profile/Profile';
 import {Dialogs} from './components/dialogs/Dialogs';
+import {Route} from 'react-router-dom';
+import {News} from './components/news/News';
+import {Music} from './components/music/Music';
+import {Settings} from './components/settings/Settings';
+
 
 function App() {
     return (
-        <div className='app-wrapper'>
+        <div className="app-wrapper">
             <Header/>
             <NavBar/>
             <div className={'app-wrapper-content'}>
-                <Profile/>
+                <Route path={'/dialogs'} component={Dialogs}/>
+                <Route path={'/profile'} component={Profile}/>
+                <Route path={'/news'} component={News}/>
+                <Route path={'/music'} component={Music}/>
+                <Route path={'/settings'} component={Settings}/>
             </div>
-            {/*<Profile/>*/}
         </div>
     );
 }
