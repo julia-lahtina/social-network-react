72784f46a72695ae7818ea1bc7569b83fc4e9a36
julia-lahtina <j.lakhtin@gmail.com>
2024-03-06 20:23:27 +0100

    create new component

---
+++ src/components/profile/profileInfo/ProfileInfo.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import s from './ProfileInfo.module.css'
+
+export const ProfileInfo = () => {
+    return (
+        <>
+            <div>
+                <img
+                    src="https://www.adorama.com/alc/wp-content/uploads/2018/11/landscape-photography-tips-yosemite-valley-feature.jpg"
+                    alt=""/>
+            </div>
+            <div>
+                ava + description
+            </div>
+        </>
+    )
+        ;
+};
