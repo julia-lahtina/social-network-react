4b652f754a83b0ac764b8789f5139f6f9e156c98
julia-lahtina <j.lakhtin@gmail.com>
2024-03-18 22:17:43 +0100

    import addPost

--- src/index.tsx
+++ src/index.tsx
@@ -4,10 +4,16 @@ import './index.css';
 import App from './App';
 import {BrowserRouter} from 'react-router-dom';
 import state from './redux/state'
+import {addPost} from './redux/state';
 
 ReactDOM.render(
     <BrowserRouter>
-        <App profilePage={state.profilePage} dialogsPage={state.dialogsPage} sidebar={state.sidebar}/>
+        <App
+            profilePage={state.profilePage}
+            dialogsPage={state.dialogsPage}
+            sidebar={state.sidebar}
+            addPost={addPost}
+        />
     </BrowserRouter>,
     document.getElementById('root')
 );
